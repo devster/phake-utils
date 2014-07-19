0b6bd3aed093f6d855610cda337a135c46f90e4c
Jeremy Perret <jeremy@devster.org>
2014-07-19 17:25:23 +0200

    improve loader file

--- load.php
+++ load.php
@@ -1,4 +1,11 @@
 <?php
 
-require_once __DIR__.'/../../vendor/autoload.php';
+$autoload = __DIR__.'/../../vendor/autoload.php';
+
+if (file_exists($autoload)) {
+    require_once $autoload;
+} else {
+    require_once __DIR__.'/src/Filesystem/Filesystem.php';
+}
+
 require_once __DIR__.'/src/Filesystem/functions.php';
